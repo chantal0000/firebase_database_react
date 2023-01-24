a1c21e71ef5989356eb49e175ab5566637f6df6f
Codingwithchantal <c_buerger@gmx.net>
2023-01-24 13:27:30 +0100

    set up firebase, adding ui, connecting db to ui successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import firebase from "./Firebase";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const ref = firebase.firestore().collection("Projects")
+    // console.log(ref);
+    const [data, setData] = useState([])
+    const [loader, setLoader] = useState(true)
+
+    function getData() {
+        ref.onSnapshot((querySnapshot) => {
+            const items = []
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data())
+            })
+            setData(items)
+            setLoader(false)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+        // console.log(data)
+    }, [])
+    return (
+        <div className="App">
+            <h1> #firebase firestore database</h1>
+            {loader === false && (data.map((dev) => (
+                <div key={dev.id}>
+                    <h1>Name: {dev.name}</h1>
+                    <p>Author: {dev.author}</p>
+                </div>
+            )))}
+        </div>
+    );
 }
 
 export default App;
